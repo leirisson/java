431cf709e1fb09a1408da31daf252e904700e953
leirisson <103783610+leirisson@users.noreply.github.com>
2025-06-15 22:29:49 -0400

    introdução a java

---
+++ introductions/helloWordDois/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        int numero = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("");
+            numero += i;
+            System.out.printf(numero + " + " + i);
+        }
+
+        System.out.println("numero total: " + numero);
+    }
+}
